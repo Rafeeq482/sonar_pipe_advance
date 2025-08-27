622a0002e87d03fb24498aeb270264e6bbf1eba5
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-08-27 13:05:37 +0530

    new commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,19 @@
 output "sonarqube_public_ip" {
-  value = aws_instance.sonarqube.public_ip
+  description = "Public IP of the SonarQube EC2 instance"
+  value       = aws_instance.sonarqube.public_ip
 }
 
 output "security_group_id" {
-  value = aws_security_group.sonarqube_sg.id
+  description = "Security Group ID used by the SonarQube instance"
+  value       = aws_security_group.sonar_sg.id
 }
 
 output "subnet_id" {
-  value = aws_subnet.sonarqube_subnet.id
+  description = "Subnet ID where SonarQube instance is deployed"
+  value       = aws_instance.sonarqube.subnet_id
+}
+
+output "instance_id" {
+  description = "Instance ID of the SonarQube EC2"
+  value       = aws_instance.sonarqube.id
 }
